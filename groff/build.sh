df01c65156489a7d8f7016bcad75a320fdada66a
Luis E Limon <9660709+llimon@users.noreply.github.com>
2026-09-10 17:02:50 -0700

    New port GNU Groff, Needed by some build tools

---
+++ groff/build.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=groff
+version=1.22.1
+pkgver=1
+source[0]=http://www.mirrorservice.org/sites/ftp.gnu.org/gnu/libidn/$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Global settings
+configure_args+=(--with-libiconv-prefix=$prefix)
+
+reg prep
+prep()
+{
+    generic_prep
+    setdir source
+}
+
+
+run_configure()
+{
+    local my_ac_overrides="$platform_ac_overrides $ac_overrides"
+    setdir ${srcdir}/${topsrcdir}/$1
+
+    local acvar
+    for acvar in $my_ac_overrides; do
+        export $acvar
+    done
+    echo $__configure "${configure_args[@]}"
+    $__configure "${configure_args[@]}"
+}
+
+
+reg build
+build()
+{
+    export CFLAGS="$CFLAGS -std=gnu99" 
+    export CXXFLAGS="$CXXFLAGS -fpermissive"
+    export LIBS="$LIBS -lsnprintf -lgcc_s"
+    ac_overrides="ac_cv_func_getprogname=yes \
+                  ac_cv_prog_cc_c11=no \
+                  gl_cv_compiler_c11_supported=no \
+                  ac_cv_func_vsnprintf=yes \
+                  ac_cv_func_snprintf=yes \
+                  gl_cv_func_vsnprintf_posix=yes \
+                  gl_cv_func_snprintf_posix=yes \
+                  gl_cv_func_vsnprintf_zerosize_bug=no"
+
+    run_configure
+    currdir = $PWD
+    ${__make} CC="${CC:-gcc}  -include ${prefix}/include/compat/snprintf_compat.h -include ${prefix}/include/compat/getprogname_compat.h" ${make_build_opts}
+
+
+}
+
+
+
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc AUTHORS COPYING* NEWS README.md
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
